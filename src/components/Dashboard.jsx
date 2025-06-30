1d2d4a96a08876daa6c640fc6d166c318a33524a
sridhar3145 <sridhar314507@gmail.com>
2025-06-30 13:37:26 +0530

    change in code

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-export default function Dashboard() {
+const Dashboard = () => {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [details, setDetails] = useState([]);
@@ -78,4 +78,5 @@ export default function Dashboard() {
       </div>
     </>
   );
-}
+};
+export default Dashboard;
